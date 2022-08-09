0a15e8f28d1cbb59888c8221ae471c42fed38841
0andme <yu00mi97@gmail.com>
2022-08-09 14:59:40 +0900

    #4 feat : 한글 입력 시 메세지 창 on

--- src/js/keyboard.js
+++ src/js/keyboard.js
@@ -3,6 +3,7 @@ export class Keyboard {
   #fontSelectEl;
   #containerEl;
   #keyboardEl;
+  #inputGroupEl;
   constructor() {
     this.#assignElement();
     this.#addEvent();
@@ -12,14 +13,21 @@ export class Keyboard {
     this.#switchEl = this.#containerEl.querySelector("#switch");
     this.#fontSelectEl = this.#containerEl.querySelector("#font");
     this.#keyboardEl = this.#containerEl.querySelector("#keyboard");
+    this.#inputGroupEl = this.#containerEl.querySelector("#input-group");
   }
   #addEvent() {
     this.#switchEl.addEventListener("change", this.#onChangeTheme);
     this.#fontSelectEl.addEventListener("change", this.#onChangeFont);
+    // 키 입력 핸들러
     document.addEventListener("keydown", (event) => {
       this.#keyboardEl
         .querySelector(`[data-code=${event.code}]`)
         ?.classList.add("active");
+      // 한글 입력 제어
+      this.#inputGroupEl.classList.toggle(
+        "error",
+        /[ㄱ-ㅎ|ㅏ-ㅣㅣ가-힣]/.test(event.key)
+      );
     });
     document.addEventListener("keyup", (event) => {
       this.#keyboardEl
@@ -27,12 +35,14 @@ export class Keyboard {
         ?.classList.remove("active");
     });
   }
+  // 테마 변경 핸들러
   #onChangeTheme(event) {
     document.documentElement.setAttribute(
       "theme",
       event.target.checked ? "dark-mode" : ""
     );
   }
+  // 폰트 변경 핸들러
   #onChangeFont(event) {
     document.body.style.fontFamily = event.target.value;
   }
