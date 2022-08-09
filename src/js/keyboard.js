5e75605362a5b7d4c4d43548ae6986cfc3bebfba
0andme <yu00mi97@gmail.com>
2022-08-09 14:44:42 +0900

    #4 feat : 키 입력시 active 클래스 추가 및 제거

--- src/js/keyboard.js
+++ src/js/keyboard.js
@@ -2,6 +2,7 @@ export class Keyboard {
   #switchEl;
   #fontSelectEl;
   #containerEl;
+  #keyboardEl;
   constructor() {
     this.#assignElement();
     this.#addEvent();
@@ -10,10 +11,21 @@ export class Keyboard {
     this.#containerEl = document.getElementById("container");
     this.#switchEl = this.#containerEl.querySelector("#switch");
     this.#fontSelectEl = this.#containerEl.querySelector("#font");
+    this.#keyboardEl = this.#containerEl.querySelector("#keyboard");
   }
   #addEvent() {
     this.#switchEl.addEventListener("change", this.#onChangeTheme);
     this.#fontSelectEl.addEventListener("change", this.#onChangeFont);
+    document.addEventListener("keydown", (event) => {
+      this.#keyboardEl
+        .querySelector(`[data-code=${event.code}]`)
+        ?.classList.add("active");
+    });
+    document.addEventListener("keyup", (event) => {
+      this.#keyboardEl
+        .querySelector(`[data-code=${event.code}]`)
+        ?.classList.remove("active");
+    });
   }
   #onChangeTheme(event) {
     document.documentElement.setAttribute(
